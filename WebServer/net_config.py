5340c8b84b18c88b99b8caef3dedf22f4aaf49ef
xianlopez <lopezalvarez.xian@gmail.com>
2018-05-31 00:15:17 +0200

    change ip

--- WebServer/net_config.py
+++ WebServer/net_config.py
@@ -6,7 +6,7 @@ class PredictConfiguration:
     model_name = 'vgg16'  # 'vgg16', 'mnistnet', 'yolo'
     # weights_file = r'.\weights\YOLO_small.ckpt'
     # weights_file = r'.\weights\vgg_16.ckpt'
-    weights_file = '/home/xian/brainlab/experiments/2018/2018_05_01_2/model-3'
+    weights_file = '/home/xian/brainlab/experiments/2018/vgg_arbores_3/model-15'
 
 
     ##################################
@@ -19,29 +19,19 @@ class PredictConfiguration:
     preprocess_opts = PreprocessOpts()
     ##################################
 
+    dropout_keep_prob = 1.0
+    gpu_memory_fraction = 0.5
 
     ##################################
     ############ RESIZING ############
-    # Select the way to fit the image to the size required by the network.
-    # For DETECTION, use ONLY RESIZE_WARP.
-    # 'resize_warp': Resize both sides of the image to the required sizes. Aspect ratio may be changed.
-    # 'resize_pad_zeros': Scale the image until it totally fits inside the required shape. We pad with zeros the areas
-    #                     in which there is no image. Aspect ratio is preserved.
-    # 'resize_lose_part': Scale the image until it totally covers the area that the network will see. We may lose the
-    #                     upper and lower parts, or the left and right ones. Aspect ratio is preserved.
-    # 'centered_crop': Take a centered crop of the image. If any dimension of the image is smaller than the input
-    #                  size, we pad with zeros.
     resize_method = 'resize_warp'  # 'resize_warp', 'resize_pad_zeros', 'resize_lose_part', 'centered_crop'
     ##################################
 
     write_images = True
     write_results = True
 
-    dataset_info_path = '/home/xian/datasets/dataset_arbores_3/dataset_info.xml'
-    # dataset_info_path = r'D:\datasets\coco-animals\dataset_info.xml'
-    # image_path = [r'C:\Users\xlopez\Desktop\cat.jpg']
-    image_path = ['/home/xian/Pictures/IMG_20180414_172848.jpg',
-                  '/home/xian/Pictures/IMG_20180414_172853.jpg']
+    dataset_info_path = '/home/xian/datasets/arbores_2018-05-30_00-09/dataset_info.xml'
+    image_path = []
     # image_path = r'D:\datasets\coco-animals\val\horse\COCO_val2014_000000492407.jpg'
 
     # Options only for detection:
@@ -65,7 +55,7 @@ class PredictConfiguration:
     ##################################
 
     num_workers = 4  # Number of parallel processes to read the data.
-    experiments_folder = r'.\experiments'
+    experiments_folder = './experiments'
     random_seed = 1234  # An integer number, or None in order not to set the random seed.
     buffer_size = 1000
 
--- crear_dataset.py
+++ crear_dataset.py
@@ -3,19 +3,47 @@ from shutil import copyfile
 import os
 from random import shuffle
 from sets import Set
+import datetime
+import cv2
 
-basedir = '/home/xian/treefinder'
+base_dir = '/home/xian/treefinder'
 data_dir = os.path.join(base_dir, 'Data')
 framesdir = os.path.join(data_dir, 'Frames')
 fotosdir = os.path.join(data_dir, 'Fotos')
-dirout = '/home/xian/datasets/dataset_arbores_3'
+dirout = '/home/xian/datasets'
+datasetname = 'arbores_' + datetime.datetime.now().strftime('%Y-%m-%d_%H-%M')
+dirout = os.path.join(dirout, datasetname)
 
-percent_frames = 10
+percent_frames = 1
 train_percent = 80
+nfotos_minimas = 20
+max_image_side = 500
 
 lista_arbores_videos = os.listdir(framesdir)
 lista_arbores_fotos= os.listdir(fotosdir)
 lista_arbores = list(Set(lista_arbores_videos) | Set(lista_arbores_fotos))
+
+# Contar numero de fotos para cada arbore:
+nfotos_arbores = dict()
+for arbore in lista_arbores_fotos:
+	nfotos_arbores[arbore] = len(os.listdir(os.path.join(fotosdir, arbore)))
+
+# Ponher como clase "unknown" as arbores sen fotos ou cun numero menor co limite:
+classes_map = dict()
+class_id = -1
+classnames = dict()
+for arbore_idx in range(len(lista_arbores)):
+	arbore = lista_arbores[arbore_idx]
+	if arbore in lista_arbores_fotos:
+		if nfotos_arbores[arbore] >= nfotos_minimas:
+			class_id += 1
+			classes_map[arbore_idx] = class_id
+			classnames[class_id] = arbore
+unknown_id = len(classes_map)
+for arbore_idx in range(len(lista_arbores)):
+	if arbore_idx not in classes_map:
+		classes_map[arbore_idx] = unknown_id
+		classnames[unknown_id] = 'unknown'
 
 path_trainlabels = os.path.join(dirout, 'train_labels.txt')
 path_vallabels = os.path.join(dirout, 'val_labels.txt')
@@ -28,10 +56,25 @@ if not os.path.exists(dirout):
 lista_train = []
 lista_val = []
 
+def copy_and_resize_img(src, dst):
+	image = cv2.imread(src)
+	h, w, _ = image.shape
+	max_side = max(w, h)
+	if max_side > max_image_side:
+		factor = float(max_image_side) / max_side
+		new_w = int(np.round(w * factor))
+		new_h = int(np.round(h * factor))
+		image = cv2.resize(image, (new_w, new_h))
+	cv2.imwrite(dst, image)
+
 # Loop over all the data:
 for arbore_idx in range(len(lista_arbores)):
 	arbore = lista_arbores[arbore_idx]
-	print(arbore + ' - ' + str(arbore_idx))
+	class_name = classnames[classes_map[arbore_idx]]
+	if class_name == 'unknown':
+		print(arbore + ' - ' + str(classes_map[arbore_idx])) + ' (unknown)'
+	else:
+		print(arbore + ' - ' + str(classes_map[arbore_idx]))
 	# Videos
 	arboredir = os.path.join(framesdir, arbore)
 	if os.path.exists(arboredir):
@@ -54,16 +97,17 @@ for arbore_idx in range(len(lista_arbores)):
 					idxs_sel = np.int16(np.linspace(0, nframes - 1, nframes_sel))
 					for i in idxs_sel:
 						path_in = os.path.join(videodir, frames[i])
-						folder_frame_out = os.path.join(dirout, splitname, arbore)
+						folder_frame_out = os.path.join(dirout, splitname, class_name)
 						if not os.path.exists(folder_frame_out):
 							os.makedirs(folder_frame_out)
-						path_out_rel = os.path.join(splitname, arbore, vista + '-' + vid + '-' + frames[i])
+						path_out_rel = os.path.join(splitname, class_name, vista + '-' + vid + '-' + frames[i])
 						path_out_abs = os.path.join(dirout, path_out_rel)
-						copyfile(path_in, path_out_abs)
+						#copyfile(path_in, path_out_abs)
+						copy_and_resize_img(path_in, path_out_abs)
 						if is_train:
-							lista_train.append(path_out_rel + ',' + str(arbore_idx))
+							lista_train.append(path_out_rel + ',' + str(classes_map[arbore_idx]))
 						else:
-							lista_val.append(path_out_rel + ',' + str(arbore_idx))
+							lista_val.append(path_out_rel + ',' + str(classes_map[arbore_idx]))
 	# Fotos
 	arboredir = os.path.join(fotosdir, arbore)
 	if os.path.exists(arboredir):
@@ -76,16 +120,17 @@ for arbore_idx in range(len(lista_arbores)):
 				is_train = False
 				splitname = 'val'
 			path_in = os.path.join(arboredir, fotoname)
-			folder_frame_out = os.path.join(dirout, splitname, arbore)
+			folder_frame_out = os.path.join(dirout, splitname, class_name)
 			if not os.path.exists(folder_frame_out):
 				os.makedirs(folder_frame_out)
-			path_out_rel = os.path.join(splitname, arbore, fotoname)
+			path_out_rel = os.path.join(splitname, class_name, fotoname)
 			path_out_abs = os.path.join(dirout, path_out_rel)
-			copyfile(path_in, path_out_abs)
+			#copyfile(path_in, path_out_abs)
+			copy_and_resize_img(path_in, path_out_abs)
 			if is_train:
-				lista_train.append(path_out_rel + ',' + str(arbore_idx))
+				lista_train.append(path_out_rel + ',' + str(classes_map[arbore_idx]))
 			else:
-				lista_val.append(path_out_rel + ',' + str(arbore_idx))
+				lista_val.append(path_out_rel + ',' + str(classes_map[arbore_idx]))
 
 # Shuffle files lists:
 shuffle(lista_train)
@@ -104,10 +149,10 @@ with open(os.path.join(dirout, 'dataset_info.xml'), 'w') as fid:
 	fid.write('<root>\n')
 	fid.write('    <format>jpg</format>\n')
 	fid.write('    <classes>\n')
-	for arbore_idx in range(len(lista_arbores)):
+	for class_id in range(len(classnames)):
 		fid.write('        <class>\n')
-		fid.write('            <id>' + str(arbore_idx) + '</id>\n')
-		fid.write('            <name>' + lista_arbores[arbore_idx] + '</name>\n')
+		fid.write('            <id>' + str(class_id) + '</id>\n')
+		fid.write('            <name>' + classnames[class_id] + '</name>\n')
 		fid.write('        </class>\n')
 	fid.write('    </classes>\n')
 	fid.write('</root>\n')
